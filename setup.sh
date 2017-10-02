895a3be0626c2c68d4274cc70940c01306fe9940
Iliana Panagopoulou <hpanago@gmail.com>
2017-10-02 20:44:23 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bash
-exec 2>/var/log/VMInit.log 2>&1
 
-if [ "$EUID" -ne 0 ]
-then
+if [ "$EUID" -ne 0 ]; then
     echo 'The script needs to be run as root.'
     # if not root exit
-    exit 0
+    exit 1
+else
+    exec 2>/var/log/VMInit.log 2>&1
 fi
 
 if [ ! -f /etc/VMInit.py ]; then
 
     if  cp ./VMInit.sh /etc/init.d/VMInit ; then
-        retval1=$?
+        echo "Copied init script from ./VMInit.sh to /etc/init.d/VMInit"
     else
 	echo "File VMInit.sh does not exist or could not be copied. Exiting."
 	exit 1
     fi
     if  cp ./VMInit.py /etc/VMInit.py ; then
-        retval2=$?
+        echo "Copied python executable ./VMInit.py to /etc/VMInit.py"
     else
 	rm /etc/init.d/VMInit
         echo "File VMInit.py does not exist or could not be copied. Exiting."
         exit 1
     fi
     if  touch /etc/vmlist ; then
-        retval3=$?
+        echo "Created file /etc/vmlist. In that file you can list all your existing vms."
     else
 	rm /etc/init.d/VMInit
 	rm /etc/VMInit.py
-        echo "File vmlist could now be created. Exiting."
+        echo "File vmlist could not be created. Exiting."
         exit 1
     fi
     
@@ -38,20 +38,7 @@ if [ ! -f /etc/VMInit.py ]; then
         update-rc.d VMInit defaults
     else
         echo "Very well. If you wish to do it later, simply run the command 'sudo update-rc.d VMInit defaults.'"
-    fi
-    #maybe there is better way for this
-    success=$(($retval1 + $retval2 + $retval3))
-
-    if [[ $success -eq 0 ]]; then
-        #some details for the user, it's always nice to know where scripts are 
-        echo "The required files have been copied successfully."
-        echo "The init script at /etc/init.d/VMInit has been enabled with update-rc.d VMInit defaults"
-        echo "The python executable resides at /etc/VMInit.py"
-        echo "Your VMs list resides at /etc/vmlist" 
-    else
-        echo "Copying files failed"
-    fi
-
+    fi  
 else
     
     echo "VMInit already exists in your system!"
