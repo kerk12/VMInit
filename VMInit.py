e2f3fc0c7227f4cbc7e3de2becbf452a956dadfb
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-12-06 03:55:56 +0200

    Can now start a specific user's VMs.

--- VMInit.py
+++ VMInit.py
@@ -9,7 +9,7 @@
 """
 
 import argparse
-import subprocess  # TODO Switch to subprocess32
+import subprocess
 import re
 import time
 
@@ -22,15 +22,29 @@ def loadVMs():
         exit(1)
     return vms
 
-def checkForRunningVM(name):
+def checkForRunningVM(name, user=None):
     # (VMs in 'VBoxManage list runningvms' use this format)
-    reg = r'\"' + name.rstrip() + '" \{\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\}'
+    reg = r'\"' + name + '" \{\w{8}-\w{4}-\w{4}-\w{4}-\w{12}\}'
     p = re.compile(reg)
-    runningvms = subprocess.check_output(["VBoxManage", "list", "runningvms"]).split("\n")
+    if user is not None:
+        runningvms = subprocess.check_output(["sudo", "-u", user, "VBoxManage", "list", "runningvms"]).split("\n")
+    else:
+        runningvms = subprocess.check_output(["VBoxManage", "list", "runningvms"]).split("\n")
+
     for vm in runningvms:
         if p.match(vm.rstrip()):
             return True
     return False
+
+def check_user(vm_string):
+    check_str = re.compile(r'(\w* ?)*::(\w)*')
+    if check_str.match(vm_string):
+        tmp = vm_string.split("::")
+        vm = tmp[0]
+        user = tmp[1]
+        return vm, user
+
+    return vm_string, None
 
 RETRIES = 10
 RETRY_TIME = 5
@@ -47,22 +61,44 @@ if args.start:
     for vm in vms:
         if vm == "":
             continue
-        subprocess.call(["VBoxManage", "startvm", vm.rstrip(), "--type", "headless"])
+
+        if vm[0] == "#":
+            continue
+
+        vm, user = check_user(vm.rstrip())
+
+        if user is not None:
+            subprocess.call(["sudo", "-u", user, "VBoxManage", "startvm", vm, "--type", "headless"])
+        else:
+            subprocess.call(["VBoxManage", "startvm", vm, "--type", "headless"])
+
 elif args.stop:
     # 1. Check every VM in the list
     for vm in vms:
-        if vm.rstrip() == "" or not checkForRunningVM(vm.rstrip()):
+        if vm == "":
+            continue
+        if vm[0] == "#":
+            continue
+
+        vm, user = check_user(vm.rstrip())
+
+        if not checkForRunningVM(name=vm, user=user):
             continue
 
         # 2. Run VBoxManage controlvm ... acpipowerbutton
-        print "Shutting down", vm.rstrip()
-        subprocess.call(["VBoxManage", "controlvm", vm.rstrip(), "acpipowerbutton"])
+        print "Shutting down", vm
+
+        if user is not None:
+            subprocess.call(["sudo", "-u", user, "VBoxManage", "controlvm", vm, "acpipowerbutton"])
+        else:
+            subprocess.call(["VBoxManage", "controlvm", vm, "acpipowerbutton"])
+
         found = True
         # 3. Repeat 4 times, 15 seconds of wait time each
         for i in range(1,RETRIES):
             # 3.4 If a running VM with the name of the shutting-down VM wasn't found, then it's shut down successfully.
             # Move to the next one...
-            if not checkForRunningVM(vm.rstrip()):
+            if not checkForRunningVM(vm, user):
                 found = False
                 break
             else:
