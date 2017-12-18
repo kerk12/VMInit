a7ee3e63700acc586a9f228e44dce3740e24a422
Kyriakos Giannakis <kerk12gr@gmail.com>
2017-12-18 18:54:29 +0200

    Added comments on test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from VMInit import loadVMs, check_user
 
 class TestVMInit(unittest.TestCase):
     def setUp(self):
+        """ Creates a custom vmlist in a temporary directory, in order for tests to be performed. """
         contents = ["VM1\n","VM2::kerk12"]
 
         self.tempdir = tempfile.mkdtemp()
@@ -15,13 +16,16 @@ class TestVMInit(unittest.TestCase):
         self.vms = loadVMs(vmlist_file=self.tempdir+"/vmlist.txt")
 
     def tearDown(self):
+        """ Deletes tempdir, once a test is complete. """
         shutil.rmtree(self.tempdir)
 
 
     def test_vmdetection(self):
+        """ Checks if the 2 VMs entered in the vmlist were detected successfully. """
         self.assertEqual(len(self.vms), 2)
 
     def test_userdetection(self):
+        """ Checks if the correct user for each VM is detected successfully. """
         vm, user = check_user(self.vms[0])
         self.assertEqual(vm, "VM1")
         self.assertEqual(user, None)
